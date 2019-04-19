381dacc3feaea4005abb2572484d7847dd012749
KieranGrist <up814853@myport.ac.uk>
2019-04-19 13:34:05 +0100

    Fixed Bullet Class

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
 //Init Structure For Bullet
-public struct BulletInit
+struct BulletInit
 {
-    public MyVector3 GunPosition; //Position Of Gun
-    public MyVector3 GunRotation; //Rotation Of Gun
-    public float FireSpeed; //Speed To fire bullet at
-       public float mass; //Weight Of Bullet
+    MyVector3 GunPosition; //Position Of Gun
+    MyVector3 GunRotation; //Rotation Of Gun
+    float FireSpeed; //Speed To fire bullet at
+    float mass; //Weight Of Bullet
 }
 
 //Class For bullet objects
-public class Bullet : MonoBehaviour {
-   
+class Bullet : MonoBehaviour
+{
 
-    public bool Alive = true; //Bool to check if the bullet is still alive 
-   public  float timeoutDestructor; //Time bullet has been alive 
-    public  myTransformation Transformation; //Transformation Component
-    public MyPhysics Physics; //Physics Component
 
+    bool Alive = true; //Bool to check if the bullet is still alive 
+    float timeoutDestructor; //Time bullet has been alive 
+    myTransformation Transformation; //Transformation Component
+    MyPhysics Physics; //Physics Component
+    float timer;
     //Used to Initialise Bullet
-    public void Init(BulletInit Initiate)
+    void Init(BulletInit Initiate)
     {
-//Get Components
+        //Get Components
         Physics = GetComponent<MyPhysics>();
         Transformation = GetComponent<myTransformation>();
 
@@ -38,11 +39,11 @@ public class Bullet : MonoBehaviour {
 
         //Set Rotation And Translation
         Transformation.Translation = Initiate.GunPosition;
-        Transformation.Rotation = Initiate.GunRotation;
+
 
         //Apply a force in the direction of the gun and times it by the firing speed
-        Physics.Force = VectorMaths.EulerAnglesToDirection(new MyVector3(Transformation.Rotation.x, Transformation.Rotation.y, 0)) * Initiate.FireSpeed;
-        Physics.Dynamic = true;
+        Physics.Force = VectorMaths.EulerAnglesToDirection(new MyVector3(Initiate.GunRotation.x, -Initiate.GunRotation.y, 0)) * Initiate.FireSpeed;
+        Physics.Bouncy = true;
         //Force Bullet to be alive
         Alive = true;
     }
@@ -50,21 +51,18 @@ public class Bullet : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-
+        timeoutDestructor = 0;
+        timer = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Transformation = GetComponent<myTransformation>();
+        Physics = GetComponent<MyPhysics>();
         //Update Components
 
         //Add Delta Time TO timeout Destructor
-    //     timeoutDestructor += Time.deltaTime;
-        if (Physics.Collided ==true)
-        {
-            Alive = false;
-        }
-
+        timeoutDestructor += Time.deltaTime;
     }
 }
